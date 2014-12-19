13f77db8ea1963f5e98755f3803743876b804f9e
Paul Jacobs <paul.jacobs@gmail.com>
2014-12-19 04:36:32 -0800

    More updates.. about to start on 02

--- commercialExtractor.py
+++ commercialExtractor.py
@@ -13,12 +13,16 @@ sr = 44100
 inputDir = 'input/'
 outputDir = 'output/'
 
+
 #inputFile = 'Safari_20141129_1443_CommercialDetection_irregular_short.aiff'
 inputFile = 'Safari_20141212_1103_CommercialDetection_pandora.aiff'
 
+# storage
+pool = essentia.Pool()
+
 print 'processing: ' + inputFile
 
-# setup modules
+# processing module setup
 audio = MonoLoader(filename = inputDir+inputFile)()
 w = Windowing(type = 'hann')
 spectrum = Spectrum()  # FFT() would return the complex FFT, here we just want the magnitude spectrum
@@ -26,47 +30,41 @@ mfcc = MFCC()
 rms = RMS()
 levels = LevelExtractor(frameSize = 20480, hopSize = 5120)
 loudness = Loudness()
+silenceThreshold = 1.0e-4
 
-# get audio
-#audio = loader()
+# processing stream
 
-## Essentia Way ##
-pool = essentia.Pool()
-
+# get levels
 pool.add('lowlevel.levels',levels(audio))
-pool.add('lowlevel.loudness',loudness(audio))
 
-for frame in FrameGenerator(audio, frameSize = 2048, hopSize = 512):
-    mfcc_bands, mfcc_coeffs = mfcc(spectrum(w(frame)))
-    pool.add('lowlevel.mfcc',mfcc_coeffs)
-    rms512 = rms(w(frame))
-    pool.add('lowlevel.rms512',rms512)
-	
+# find silent frames
+pool.add('silenceDetected',essentia.array(pool['lowlevel.levels'][0] < silenceThreshold))
 
-# and plot
-# t = linspace(0, (audio.size/5167.96), pool['lowlevel.rms512'].size)
+# find beginning and end frames of silence periods
+sil = pool['silenceDetected'];
+it = nditer([sil, None], 
+       flags=['c_index', 'refs_ok'],
+       op_flags=[['readonly'],['writeonly','allocate']])
 
-figure(1)
-subplot(2,1,1)
-plot(pool['lowlevel.rms512'])
-subplot(2,1,2)
-plot(audio)
-show()
+if (it.index == 0):
+    it[1] = 0
+    it.iternext()
 
-plot(pool['lowlevel.loudness'])
-show()
-figure()
+while not it.finished:
+    if (sil[it.index] == 1.0): # preserve edges
+        if (sil[it.index-1] == 0):
+            it[1] = 1; # beginning of silence
+        elif (sil[it.index+1] == 0):
+            it[1] = 2; # end of silence
+        else:
+            it[1] = 0; # no change observed
+    else:
+        it[1] = 0;
+    it.iternext()
 
-plot(pool['lowlevel.levels'])
-show()
-figure()
+pool.add('silenceEdges',it.operands[1]);
 
-#imshow(pool['lowlevel.mfcc_bands'].T, aspect='auto', interpolation='nearest')
-#show() # unnecessary if you started "ipython --pylab"
-#figure()
-
-imshow(pool['lowlevel.mfcc'].T[1:,:], aspect = 'auto')
-show() # unnecessary if you started "ipython --pylab"
+YamlOutput(filename = outputDir + inputFile + '.silences.yaml')(pool['silenceEdges'])
 
 aggPool = PoolAggregator(defaultStats = ['mean','var'])(pool)
 YamlOutput(filename = outputDir + inputFile + '.features.yaml')(aggPool)
