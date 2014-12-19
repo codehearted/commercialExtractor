2012c3bfd73a29a81a8f1033702a6c9d8d9f2a63
Paul Jacobs <paul.jacobs@gmail.com>
2014-12-19 07:59:58 -0800

    Pretty much working now.. just have to let it take an arg for the file.

--- commercialExtractor.py
+++ commercialExtractor.py
@@ -6,21 +6,52 @@
 from essentia.standard import *
 from pylab import plot, show, figure, imshow, axis, subplot
 from numpy import *
+from ordereddict import OrderedDict
+import yaml
 
+
+# config constants
+silenceThreshold = 1.0e-4
+commercialLengthVariance = 0.2
+commercialUnitLength = 15.0
+minimumCommercialLength = 7.5
 
 #constants
 sr = 44100
 inputDir = 'input/'
 outputDir = 'output/'
 
+#hopSz = 5120 ## too big - innacurate start times for commercials
+#hopSz = 2048 ## too small - split the third commercial into 3 pieces
+hopSz = 3000
 
-#inputFile = 'Safari_20141129_1443_CommercialDetection_irregular_short.aiff'
-inputFile = 'Safari_20141212_1103_CommercialDetection_pandora.aiff'
+frameDuration = (hopSz * 1.0) / sr
+
+# converters for time
+def framesToSeconds(frame):
+	return frame*frameDuration;
+
+def secondsToTime(sec):
+	return str(int(sec/60))+':'+str(trunc((sec%60.0)*10)*0.1)+'/'+str(int(sec*10)*.1)+'/'+str(int(sec/frameDuration))
+
+def secondsToShortTime(sec):
+	return str(int(sec/60))+':'+str(trunc((sec%60.0)*10)*0.1)
+
+
+def framesToTime(frame):
+	return secondsToTime(framesToSeconds(frame))
+
+
+### Tested and Working Great
+#inputFile = 'Safari_20141212_1103_CommercialDetection_pandora.aiff'
+#inputFile = 'Safari_20141219_0353_CommercialDetection_pandora.aiff'
+
+inputFile = 'Safari_20141129_1443_CommercialDetection_irregular_short.aiff'
 
 # storage
 pool = essentia.Pool()
 
-print 'processing: ' + inputFile
+#print 'processing: ' + inputFile
 
 # processing module setup
 audio = MonoLoader(filename = inputDir+inputFile)()
@@ -28,9 +59,9 @@ w = Windowing(type = 'hann')
 spectrum = Spectrum()  # FFT() would return the complex FFT, here we just want the magnitude spectrum
 mfcc = MFCC()
 rms = RMS()
-levels = LevelExtractor(frameSize = 20480, hopSize = 5120)
+levels = LevelExtractor(frameSize = hopSz*4, hopSize = hopSz)
 loudness = Loudness()
-silenceThreshold = 1.0e-4
+
 
 # processing stream
 
@@ -41,31 +72,73 @@ pool.add('lowlevel.levels',levels(audio))
 pool.add('silenceDetected',essentia.array(pool['lowlevel.levels'][0] < silenceThreshold))
 
 # find beginning and end frames of silence periods
-sil = pool['silenceDetected'];
+sil = pool['silenceDetected'][0];
+silences = OrderedDict()
+silencesInSecs = OrderedDict()
+
 it = nditer([sil, None], 
        flags=['c_index', 'refs_ok'],
        op_flags=[['readonly'],['writeonly','allocate']])
 
-if (it.index == 0):
-    it[1] = 0
-    it.iternext()
+
+#if (it.index == 0):
+#    it[1] = 0
+#    it.iternext()
 
 while not it.finished:
     if (sil[it.index] == 1.0): # preserve edges
-        if (sil[it.index-1] == 0):
+        if (it.index > 0 and sil[it.index-1] == 0):
             it[1] = 1; # beginning of silence
-        elif (sil[it.index+1] == 0):
+            silences[it.index] = 1
+            silencesInSecs[framesToTime(it.index)] = 1
+        elif (it.index < (sil.shape[0]-1) and sil[it.index+1] == 0):
             it[1] = 2; # end of silence
+            silences[it.index] = 2
+            silencesInSecs[framesToTime(it.index)] = 2
         else:
             it[1] = 0; # no change observed
     else:
         it[1] = 0;
     it.iternext()
 
-pool.add('silenceEdges',it.operands[1]);
+silenceEdges = it.operands[1]
+pool.add('silenceEdges',silenceEdges)
 
-YamlOutput(filename = outputDir + inputFile + '.silences.yaml')(pool['silenceEdges'])
+segments = OrderedDict()
+commercials = OrderedDict()
 
-aggPool = PoolAggregator(defaultStats = ['mean','var'])(pool)
-YamlOutput(filename = outputDir + inputFile + '.features.yaml')(aggPool)
+#thisSilenceStartTime = 0;
+lastSilenceFinishTime = 0;
+lastSilenceFinishTimeInSeconds = 0.0;
+soundDuration = 0;
+soundDurationInSeconds = 0.0;
+for timeIndex in sorted(silences):
+#	print "timeIndex "+framesToTime(timeIndex)
+	edgeType = silences[timeIndex]
+	if edgeType == 1: # start of silence
+#		thisSilenceStartTime = timeIndex
+		soundDuration = thisSilenceStartTime - lastSilenceFinishTime
+		soundDurationInSeconds = framesToSeconds(soundDuration)
+		segments[secondsToShortTime(framesToSeconds(lastSilenceFinishTime))] = secondsToShortTime(framesToSeconds(soundDuration))
+#		print "segment starting "+framesToTime(lastSilenceFinishTime)+" - "+str(soundDurationInSeconds)+"\n"
+#		segments[lastSilenceFinishTime] = soundDurationInSeconds
+#		segments[secondsToTime(lastSilenceFinishTimeInSeconds)] = secondsToTime(soundDurationInSeconds)
+		if ((soundDurationInSeconds <= commercialUnitLength * 5) and \
+			(soundDurationInSeconds > minimumCommercialLength) and \
+			((soundDurationInSeconds % commercialUnitLength) <= \
+				commercialUnitLength*commercialLengthVariance or \
+				((soundDurationInSeconds % commercialUnitLength) >= \
+				(commercialUnitLength - commercialUnitLength*commercialLengthVariance)))):
+					commercials[framesToTime(lastSilenceFinishTime)] = secondsToShortTime(soundDurationInSeconds)
+#			commercials[str(lastSilenceFinishTime)] = soundDurationInSeconds
+#			commercials[secondsToTime(lastSilenceFinishTimeInSeconds)] = secondsToTime(soundDurationInSeconds)
+	if edgeType == 2: # end of silence
+		lastSilenceFinishTime = timeIndex;
+		lastSilenceFinishTimeInSeconds = framesToSeconds(lastSilenceFinishTime)
+
+#YamlOutput(filename = outputDir + inputFile + '.silences.yaml')([pool['silenceEdges'],{'silences':silences, 'segments':segments, 'commercials':commercials}])
+print yaml.dump({'silences':silences, 'segments':segments, 'commercials':commercials})
+
+#aggPool = PoolAggregator(defaultStats = ['mean','var'])(pool)
+#YamlOutput(filename = outputDir + inputFile + '.features.yaml')(aggPool)
 
